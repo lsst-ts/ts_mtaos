3742f0f37b7cb1bb0f644a1534f803d62068a702
Tiago Ribeiro <tribeiro@lsst.org>
2021-12-17 09:17:03 -0700

    In `MTAOS.do_runWEP`, implement mechanism to differentiate wep runs using private identity (who sent the command?) and the send timestamp.

--- python/lsst/ts/MTAOS/mtaos.py
+++ python/lsst/ts/MTAOS/mtaos.py
@@ -34,6 +34,7 @@ import numpy as np
 from lsst.ts import salobj
 from lsst.ts.idl.enums.MTAOS import FilterType
 from lsst.ts.ofc import OFCData
+from lsst.ts.utils import astropy_time_from_tai_unix
 
 from . import CONFIG_SCHEMA, TELESCOPE_DOF_SCHEMA
 from . import Config
@@ -414,12 +415,22 @@ class MTAOS(salobj.ConfigurableCsc):
         else:
             # TODO (DM-31365): Remove workaround to visitId being of type long
             # in MTAOS runWEP command.
+            snd_stamp_isot = (
+                astropy_time_from_tai_unix(data.private_sndStamp)
+                .isot.replace("-", "")
+                .replace(":", "")
+                .replace(".", "")
+            )
+            private_identity = data.private_identity.replace("@", "_").replace("-", "_")
+
+            run_name_extention = f"_{private_identity}_{snd_stamp_isot}"
             await self.model.run_wep(
                 visit_id=self.visit_id_offset + data.visitId,
                 extra_id=self.visit_id_offset + data.extraId
                 if data.extraId > 0
                 else None,
                 config=yaml.safe_load(data.config) if len(data.config) > 0 else {},
+                run_name_extention=run_name_extention,
                 log_time=self.execution_times,
             )
 
