d625715ba414e3d1247536e1a6538c97a9676585
gmegh <guillem.megias.homar@gmail.com>
2025-03-27 16:54:16 -0700

    Updating config schema version

--- python/lsst/ts/mtaos/config_schema.py
+++ python/lsst/ts/mtaos/config_schema.py
@@ -36,7 +36,7 @@ $schema: http://json-schema.org/draft-07/schema#
 $id: https://github.com/lsst-ts/ts_MTAOS/blob/master/python/lsst/ts/MTAOS/schema_config.py
 # title must end with one or more spaces followed by the schema version, which
 # must begin with "v"
-title: MTAOS v5
+title: MTAOS v6
 description: Schema for MTAOS configuration files
 type: object
 
