1a27b637901a7d4a5108d20f2a767efd6895a860
gmegh <guillem.megias.homar@gmail.com>
2025-06-11 17:01:56 -0700

    Mypy ignore conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -4,16 +4,16 @@ This configuration only affects single-package Sphinx documentation builds.
 """
 
 import lsst.ts.mtaos  # noqa
-from documenteer.conf.pipelinespkg import *  # noqa
+from documenteer.conf.pipelinespkg import *  # type: ignore # noqa
 
 project = "ts_mtaos"
-html_theme_options["logotext"] = project  # noqa
+html_theme_options["logotext"] = project  # type: ignore # noqa
 html_title = project
 html_short_title = project
 doxylink = {}  # Avoid warning: Could not find tag file _doxygen/doxygen.tag
 
-intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)  # noqa
-intersphinx_mapping["ts_salobj"] = ("https://ts-salobj.lsst.io", None)  # noqa
+intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)  # type: ignore # noqa
+intersphinx_mapping["ts_salobj"] = ("https://ts-salobj.lsst.io", None)  # type: ignore # noqa
 
 # Support the sphinx extension of plantuml
 extensions.append("sphinxcontrib.plantuml")  # noqa
