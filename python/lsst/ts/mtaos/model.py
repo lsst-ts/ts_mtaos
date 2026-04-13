706134373b3ea54281c1ee585fd6194eada6fd77
David Sanmartim <davidsanm@gmail.com>
2026-04-13 16:07:09 -0400

    Fix `vmodes_selected` handler to accept None for restoration
    The `set_ofc_data_values` handler for `vmodes_selected` must accept `None` to support the save/restore cycle in _execute_ofc. When the original value is `None` (automatic truncation), restoring it requires passing `None` back through the handler. Empty lists are still skipped.

--- python/lsst/ts/mtaos/model.py
+++ python/lsst/ts/mtaos/model.py
@@ -1869,7 +1869,7 @@ class Model:
                     elif key == "zn_selected" and len(kwargs[key]) > 0:
                         self.ofc.ofc_data.zn_selected = np.array(kwargs[key])
 
-                    elif key == "vmodes_selected" and len(kwargs[key]) > 0:
+                    elif key == "vmodes_selected" and (kwargs[key] is None or len(kwargs[key]) > 0):
                         original_ofc_data_values[key] = (
                             self.ofc.ofc_data.vmodes_selected.copy()
                             if self.ofc.ofc_data.vmodes_selected is not None
